628a08a78669ab2a71ee965e374ad49b63b23ef3
Chandini Chanu <chandini180@gmail.com>
2021-01-27 15:34:59 +0530

    devlop branch

--- src/main/java/com/example/demo/GitPractiseApplication.java
+++ src/main/java/com/example/demo/GitPractiseApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitPractiseApplication {
+int y=10;
 
 	public static void main(String[] args) {
                        int z=13;
