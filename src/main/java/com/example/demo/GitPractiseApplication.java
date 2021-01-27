0a176bcd2af813f02cfdfbe3740d3f47890bcb5b
Chandini Chanu <chandini180@gmail.com>
2021-01-27 14:27:24 +0530

    feature-200

--- src/main/java/com/example/demo/GitPractiseApplication.java
+++ src/main/java/com/example/demo/GitPractiseApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitPractiseApplication {
 
 	public static void main(String[] args) {
-                         int a=10;
-                       int b=20;
+                       int z=13;
+
 		SpringApplication.run(GitPractiseApplication.class, args);
 	}
 
